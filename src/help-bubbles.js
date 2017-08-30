b7902564ee146dd153adfc8e9f5545ddc5a85735
marcelglaeser <marcelglaeser@users.noreply.github.com>
2017-08-30 20:43:27 +0200

    Center position and orientations
    The bubble have new prop "center" with values "horizontal", "vertical" and "horizontal_vertical" and the bubble orientation change now by device rotation.

--- src/help-bubbles.js
+++ src/help-bubbles.js
@@ -16,7 +16,6 @@ import React, {
 import RootSiblings from 'react-native-root-siblings'
 
 const { width, height } = Dimensions.get('window');
-const [ CLIENT_WIDTH, CLIENT_HEIGHT ] = [ width, height ]
 const DEFAULT_GROUP = 'DEFAULT_GROUP';
 const DEFAULT_SHOWSTEPNUM = true;
 const DEFAULT_STOPBTNTITLE = 'stop';
@@ -41,7 +40,7 @@ export default class HelpBubbles extends Component {
   }
 
   componentDidMount() {
-    const { group, showStepNum, step, content, disable, customToolTipStyle, customArrowStyle, stopBtnTitle, nextBtnTitle, prevBtnTitle } = this.props;
+    const { group, showStepNum, step, content, disable, center, customToolTipStyle, customArrowStyle, stopBtnTitle, nextBtnTitle, prevBtnTitle } = this.props;
 
     if (!groupMap[group || DEFAULT_GROUP]) {
       groupMap[group || DEFAULT_GROUP] = {};
@@ -53,6 +52,7 @@ export default class HelpBubbles extends Component {
       groupA[step] = {
         content,
         disable,
+        center,
         customToolTipStyle,
         customArrowStyle,
         stopBtnTitle,
@@ -67,32 +67,32 @@ export default class HelpBubbles extends Component {
     }
   }
 
-measure() {
-  this._refHelpBubbles.measure.apply(this._refHelpBubbles, arguments)
-}
+  measure() {
+    this._refHelpBubbles.measure.apply(this._refHelpBubbles, arguments)
+  }
 
-componentWillUnmount() {
-  const { group, step, content, disable, customToolTipStyle, customArrowStyle, stopBtnTitle, nextBtnTitle, prevBtnTitle } = this.props;
-  var groupA = groupMap[group || DEFAULT_GROUP][step];
+  componentWillUnmount() {
+    const { group, step, content, disable, center, customToolTipStyle, customArrowStyle, stopBtnTitle, nextBtnTitle, prevBtnTitle } = this.props;
+    var groupA = groupMap[group || DEFAULT_GROUP][step];
 
-  if (groupA._index > 0) {
-    --groupA._index;
-  } else {
-    delete groupMap[group || DEFAULT_GROUP][step]
-    if (Object.keys(groupMap[group || DEFAULT_GROUP]).length === 0) {
-      delete groupMap[group && DEFAULT_GROUP];
+    if (groupA._index > 0) {
+      --groupA._index;
+    } else {
+      delete groupMap[group || DEFAULT_GROUP][step]
+      if (Object.keys(groupMap[group || DEFAULT_GROUP]).length === 0) {
+        delete groupMap[group && DEFAULT_GROUP];
+      }
     }
   }
-}
 
-render() { //TouchableOpacity
-  this.html = (
-    <View activeOpacity={1} {...this.props} ref={c => this._refHelpBubbles = c}>
-    {this.props.children}
-    </View>
-  );
-  return this.html
-}
+  render() { //TouchableOpacity
+    this.html = (
+      <View activeOpacity={1} {...this.props} ref={c => this._refHelpBubbles = c}>
+      {this.props.children}
+      </View>
+    );
+    return this.html
+  }
 }
 
 var hilightBox = null;
@@ -191,6 +191,7 @@ export function helpbubbles(opts = {}) {
   function toStep(index) {
     var currentStep = group[stepArr[index]];
     var content = currentStep.content;
+    var center = currentStep.center;
     var customToolTipStyle = currentStep.customToolTipStyle;
     var customArrowStyle = currentStep.customArrowStyle;
     var stopBtnTitle = currentStep.stopBtnTitle;
@@ -227,6 +228,7 @@ export function helpbubbles(opts = {}) {
       refModal.innerElement = element;
       refModal.currentStep = index+1;
       refModal.content = typeof content === 'string' ? <View><Text style={styles.toolTipContent}>{content}</Text></View> : content;
+      refModal.center = center;
       refModal.customToolTipStyle = customToolTipStyle;
       refModal.customArrowStyle = customArrowStyle;
       refModal.stopBtnTitle = stopBtnTitle;
@@ -244,7 +246,8 @@ export function helpbubbles(opts = {}) {
       width: res.width + offsetW,
       height: res.height + offsetW,
       left: res.pageX - offsetW/2,
-      top: res.pageY - offsetW/2
+      top: res.pageY - offsetW/2,
+      center: center
     })
   });
 
@@ -286,280 +289,304 @@ class HelpBubblesModal extends Component {
     left: this.pageX - offsetW/2,
     top: this.pageY - offsetW/2
   })*/
-/*  this.setState({
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  });
-  */
-  //this._refHelpBubbles.style.width = Dimensions.get('window').width;
-  //alert(Dimensions.get('window').width);
-  //this.props.refresh();
+  /*  this.setState({
+  width: Dimensions.get('window').width,
+  height: Dimensions.get('window').height
+});
+*/
+//this._refHelpBubbles.style.width = Dimensions.get('window').width;
+//alert(Dimensions.get('window').width);
+//this.props.refresh();
 }
 
-  animateMove(obj = {}) {
-    var duration = 300;
+animateMove(obj = {}) {
+  var duration = 300;
 
-    var stepNumLeft = obj.left - 12;
+  var stepNumLeft = obj.left - 12;
 
-    if (stepNumLeft < 0) {
-      stepNumLeft = obj.left + obj.width - 12;
-      if (stepNumLeft > CLIENT_WIDTH - 24) {
-        stepNumLeft = CLIENT_WIDTH - 24;
-      }
+  if (stepNumLeft < 0) {
+    stepNumLeft = obj.left + obj.width - 12;
+    if (stepNumLeft > Dimensions.get('window').width - 24) {
+      stepNumLeft = Dimensions.get('window').width - 24;
     }
+  }
 
-    Animated.parallel([
-      Animated.timing(this._aniWidth, {
-        duration,
-        toValue: obj.width
-      }),
+  Animated.parallel([
+    Animated.timing(this._aniWidth, {
+      duration,
+      toValue: obj.width
+    }),
 
-      Animated.timing(this._aniHeight, {
-        duration,
-        toValue: obj.height
-      }),
+    Animated.timing(this._aniHeight, {
+      duration,
+      toValue: obj.height
+    }),
 
-      Animated.timing(this._aniLeft, {
-        duration,
-        toValue: obj.left
-      }),
+    Animated.timing(this._aniLeft, {
+      duration,
+      toValue: obj.left
+    }),
 
-      Animated.timing(this._aniTop, {
-        duration,
-        toValue: obj.top
-      }),
+    Animated.timing(this._aniTop, {
+      duration,
+      toValue: obj.top
+    }),
 
-      Animated.timing(this._aniStepNumLeft, {
-        duration,
-        toValue: stepNumLeft
-      })
-    ]).start();
+    Animated.timing(this._aniStepNumLeft, {
+      duration,
+      toValue: stepNumLeft
+    })
+  ]).start();
 
-    var centerPoint = {
-      x: obj.left + obj.width/2,
-      y: obj.top + obj.height/2
+  var centerPoint = {
+    x: obj.left + obj.width/2,
+    y: obj.top + obj.height/2
+  };
+
+  var relative2Left = centerPoint.x;
+  var relative2Top = centerPoint.y;
+  var relative2Bottom = Math.abs(centerPoint.y - Dimensions.get('window').height);
+  var relative2Right = Math.abs(centerPoint.x - Dimensions.get('window').width);
+
+  var whereVerticalPlace = relative2Bottom > relative2Top ? 'bottom' : 'top';
+  var whereHorizontalPlace = relative2Left > relative2Right ? 'left' : 'right';
+  if(obj.center == 'vertical' || obj.center == 'horizontal_vertical' ) {
+    whereVerticalPlace = 'center';
+  }
+  if(obj.center == 'horizontal' || obj.center == 'horizontal_vertical' ) {
+    whereHorizontalPlace = 'center';
+  }
+
+  var margin = 13;
+  var tooltip = {};
+  var arrow = {};
+  var container = {};
+
+  switch (whereVerticalPlace) {
+
+    case 'bottom':
+    tooltip.top = obj.top + obj.height + margin;
+    arrow.borderBottomColor = '#58b1fc';
+    arrow.top = tooltip.top - margin + 3;
+    break;
+
+    case 'top':
+    tooltip.bottom = Dimensions.get('window').height - obj.top + margin;
+    arrow.borderTopColor = '#58b1fc';
+    arrow.bottom = tooltip.bottom - margin + 3;
+    break;
+
+    case 'center':
+    this.container = {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
     };
+    break;
 
-    var relative2Left = centerPoint.x;
-    var relative2Top = centerPoint.y;
-    var relative2Bottom = Math.abs(centerPoint.y - CLIENT_HEIGHT);
-    var relative2Right = Math.abs(centerPoint.x - CLIENT_WIDTH);
-
-
-    var whereVerticalPlace = relative2Bottom > relative2Top ? 'bottom' : 'top';
-    var whereHorizontalPlace = relative2Left > relative2Right ? 'left' : 'right';
-
-    var margin = 13;
-    var tooltip = {};
-    var arrow = {};
-
-    switch (whereVerticalPlace) {
-
-      case 'bottom':
-      tooltip.top = obj.top + obj.height + margin;
-      arrow.borderBottomColor = '#58b1fc';
-      arrow.top = tooltip.top - margin + 3;
-      break;
-
-      case 'top':
-      tooltip.bottom = CLIENT_HEIGHT - obj.top + margin;
-      arrow.borderTopColor = '#58b1fc';
-      arrow.bottom = tooltip.bottom - margin + 3;
-      break;
-      default:
-      // nothing todo
-    }
-
-    switch (whereHorizontalPlace) {
-
-      case 'left':
-      tooltip.right = Math.max(CLIENT_WIDTH - (obj.left + obj.width), 0);
-      tooltip.right = tooltip.right === 0 ? tooltip.right + margin : tooltip.right;
-      tooltip.maxWidth = CLIENT_WIDTH - tooltip.right;
-      arrow.right = tooltip.right + margin;
-      break;
-
-      case 'right':
-      tooltip.left = Math.max(obj.left, 0);//obj.width*0.33;
-      tooltip.left = tooltip.left === 0 ? tooltip.left + margin : tooltip.left;
-      //alert(obj.left + obj.width);
-      tooltip.maxWidth = CLIENT_WIDTH - tooltip.left - margin;
-      arrow.left = tooltip.left+ margin;//*1.5
-      break;
-      default:
-      // nothing todo
-    }
-
-    this.tooltip = tooltip;
-    this.arrow = arrow;
+    default:
+    // nothing todo
   }
 
-  toggleTooltip(isShow = true) {
-    Animated.timing(this._aniOpacity, {
-      toValue: isShow ? 1 : 0,
-      duration: 200
-    }).start();
+  switch (whereHorizontalPlace) {
+
+    case 'left':
+    tooltip.right = Math.max(Dimensions.get('window').width - (obj.left + obj.width), 0);
+    tooltip.right = tooltip.right === 0 ? tooltip.right + margin : tooltip.right;
+    tooltip.maxWidth = Dimensions.get('window').width - tooltip.right;
+    arrow.right = tooltip.right + margin;
+    break;
+
+    case 'right':
+    tooltip.left = Math.max(obj.left, 0);//obj.width*0.33;
+    tooltip.left = tooltip.left === 0 ? tooltip.left + margin : tooltip.left;
+    //alert(obj.left + obj.width);
+    tooltip.maxWidth = Dimensions.get('window').width - tooltip.left - margin;
+    arrow.left = tooltip.left+ margin;//*1.5
+    break;
+
+    case 'center':
+    this.container = {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    };
+    break;
+
+    default:
+    // nothing todo
   }
 
-  render() {
-    return (
-      <View style={[styles.container, {zIndex}]}>
-      <Animated.View ref={(c) => this.refHilightBox = c}
-      style={[styles.hilightBox, {position: 'absolute'}, {
-        width: this._aniWidth,
-        height: this._aniHeight,
-        left: this._aniLeft,
-        top: this._aniTop
+  this.tooltip = tooltip;
+  this.arrow = arrow;
+}
+
+toggleTooltip(isShow = true) {
+  Animated.timing(this._aniOpacity, {
+    toValue: isShow ? 1 : 0,
+    duration: 200
+  }).start();
+}
+
+render() {
+  return (
+    <View style={[styles.container, this.container, {zIndex}]}>
+    <Animated.View ref={(c) => this.refHilightBox = c}
+    style={[styles.hilightBox, {position: 'absolute'}, {
+      width: this._aniWidth,
+      height: this._aniHeight,
+      left: this._aniLeft,
+      top: this._aniTop
+    }]}>
+    </Animated.View>
+    <View ref={(c) => this.refContainer = c }
+    style={[styles.modalContent]}
+    />
+    {this.props.showStepNum ?
+      <Animated.View style={[styles.stepNum, {zIndex: zIndex+1000}, {
+        left: this._aniStepNumLeft,
+        top: Animated.add(this._aniTop, -12)
       }]}>
+      <Text style={[styles.stepNumText]}>{this.currentStep}</Text>
       </Animated.View>
-      <View ref={(c) => this.refContainer = c }
-      style={[styles.modalContent]}
-      />
-      {this.props.showStepNum ?
-        <Animated.View style={[styles.stepNum, {zIndex: zIndex+1000}, {
-          left: this._aniStepNumLeft,
-          top: Animated.add(this._aniTop, -12)
-        }]}>
-        <Text style={[styles.stepNumText]}>{this.currentStep}</Text>
-        </Animated.View>
-        : null
-      }
-      <Animated.View style={[styles.arrow, this.arrow, {opacity: this._aniOpacity}, this.customArrowStyle]}></Animated.View>
-      <Animated.View style={[styles.toolTip, this.tooltip, {opacity: this._aniOpacity}, this.customToolTipStyle]}>
-      <View style={{flex: 1}}>
-      {this.content || null}
-      </View>
-      <View style={[styles.helpbubblesBar]}>
-      <View>
-      <View style={[styles.helpbubblesButton, {
-        borderColor: '#fff',
-        marginRight: 20,
+      : null
+    }
+    <Animated.View style={[styles.arrow, this.arrow, {opacity: this._aniOpacity}, this.customArrowStyle]}></Animated.View>
+    <Animated.View style={[styles.toolTip, this.tooltip, {opacity: this._aniOpacity}, this.customToolTipStyle]}>
+    <View style={{flex: 1}}>
+    {this.content || null}
+    </View>
+    <View style={[styles.helpbubblesBar]}>
+    <View>
+    <View style={[styles.helpbubblesButton, {
+      borderColor: '#fff',
+      marginRight: 20,
 
-      }]} onTouchStart={() => this.props.stop()}>
-      <Text style={[styles.buttonText, {
-        color: '#fff'
-      }]}>{this.stopBtnTitle ? this.stopBtnTitle : this.props.stopBtnTitle}</Text>
+    }]} onTouchStart={() => this.props.stop()}>
+    <Text style={[styles.buttonText, {
+      color: '#fff'
+    }]}>{this.stopBtnTitle ? this.stopBtnTitle : this.props.stopBtnTitle}</Text>
+    </View>
+    </View>
+    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
+    {this.currentStep > 1 ?
+      <View style={[styles.helpbubblesButton]} onTouchStart={() => this.props.prev()}>
+      <Text style={[styles.buttonText]}>{this.prevBtnTitle ? this.prevBtnTitle : this.props.prevBtnTitle}</Text>
       </View>
-      </View>
-      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-      {this.currentStep > 1 ?
-        <View style={[styles.helpbubblesButton]} onTouchStart={() => this.props.prev()}>
-        <Text style={[styles.buttonText]}>{this.prevBtnTitle ? this.prevBtnTitle : this.props.prevBtnTitle}</Text>
+      : null }
+      {this.currentStep < this.props.len ?
+        <View style={[styles.helpbubblesButton, {marginLeft: 8, alignSelf: 'flex-end'}]} onTouchStart={() => this.props.next()}>
+        <Text style={[styles.buttonText]}>{this.nextBtnTitle ? this.nextBtnTitle : this.props.nextBtnTitle}</Text>
         </View>
         : null }
-        {this.currentStep < this.props.len ?
-          <View style={[styles.helpbubblesButton, {marginLeft: 8, alignSelf: 'flex-end'}]} onTouchStart={() => this.props.next()}>
-          <Text style={[styles.buttonText]}>{this.nextBtnTitle ? this.nextBtnTitle : this.props.nextBtnTitle}</Text>
-          </View>
-          : null }
-          </View>
-          </View>
-          </Animated.View>
-          </View>
-        );
-      }
+        </View>
+        </View>
+        </Animated.View>
+        </View>
+      );
     }
+  }
 
-    const styles = StyleSheet.create({
-      container: {
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom:0
-      },
+  const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom:0
+    },
 
-      hilightBox: {
-        position: 'absolute',
-        backgroundColor: 'transparent',
-        borderRadius: 1,
-      },
+    hilightBox: {
+      position: 'absolute',
+      backgroundColor: 'transparent',
+      borderRadius: 1,
+    },
 
-      arrow: {
-        position: 'absolute',
-        borderColor: 'transparent',
-        borderWidth: 5
-      },
+    arrow: {
+      position: 'absolute',
+      borderColor: 'transparent',
+      borderWidth: 5
+    },
 
-      up: {
-        borderBottomColor: '#58b1fc'
-      },
+    up: {
+      borderBottomColor: '#58b1fc'
+    },
 
-      down: {
-        borderTopColor: '#58b1fc'
-      },
+    down: {
+      borderTopColor: '#58b1fc'
+    },
 
-      toolTipOuter: {
-        position: 'absolute',
-        minWidth: 180,
-        maxWidth: 300
-      },
+    toolTipOuter: {
+      position: 'absolute',
+      minWidth: 180,
+      maxWidth: 300
+    },
 
-      toolTip: {
-        position: 'absolute',
-        padding: 5,
-        backgroundColor: "#58b1fc",
-        borderRadius: 3,
-        overflow: 'hidden'
-      },
+    toolTip: {
+      position: 'absolute',
+      padding: 5,
+      backgroundColor: "#58b1fc",
+      borderRadius: 3,
+      overflow: 'hidden'
+    },
 
-      toolTipContent: {
-        fontWeight: 'bold',
-        color: '#fff'
-      },
+    toolTipContent: {
+      fontWeight: 'bold',
+      color: '#fff'
+    },
 
-      stepNum: {
-        position: 'absolute',
-        width: 24,
-        height: 24,
-        borderWidth: 2,
-        borderRadius: 12,
-        borderColor: '#fff',
-        backgroundColor: "#58b1fc",
-        overflow: 'hidden',
-        alignItems: 'center',
-        justifyContent: 'center'
-      },
+    stepNum: {
+      position: 'absolute',
+      width: 24,
+      height: 24,
+      borderWidth: 2,
+      borderRadius: 12,
+      borderColor: '#fff',
+      backgroundColor: "#58b1fc",
+      overflow: 'hidden',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
 
-      stepNumText: {
-        backgroundColor: 'rgba(255,255,255,0)',
-        fontWeight: 'bold',
-        color: '#fff'
-      },
+    stepNumText: {
+      backgroundColor: 'rgba(255,255,255,0)',
+      fontWeight: 'bold',
+      color: '#fff'
+    },
 
-      sibling: {
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        right: 0,
-        bottom: 0,
-        backgroundColor: 'transparent'
-      },
+    sibling: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'transparent'
+    },
 
-      helpbubblesBar: {
-        marginTop: 15,
-        flexDirection: 'row',
-      },
+    helpbubblesBar: {
+      marginTop: 15,
+      flexDirection: 'row',
+    },
 
-      helpbubblesButton: {
-        padding: 8,
-        borderColor: '#fff',
-        borderWidth: 1,
-        borderRadius: 2
-      },
+    helpbubblesButton: {
+      padding: 8,
+      borderColor: '#fff',
+      borderWidth: 1,
+      borderRadius: 2
+    },
 
-      buttonText: {
-        fontSize: 12,
-        textAlign: 'center',
-        color: '#fff'
-      },
+    buttonText: {
+      fontSize: 12,
+      textAlign: 'center',
+      color: '#fff'
+    },
 
-      modalContent: {
-        position: 'absolute',
-        overflow: 'hidden'
-      },
+    modalContent: {
+      position: 'absolute',
+      overflow: 'hidden'
+    },
 
-      modal: {
-        backgroundColor: 'rgba(0,0,0,0.5)',
-      }
-    });
+    modal: {
+      backgroundColor: 'rgba(0,0,0,0.5)',
+    }
+  });
